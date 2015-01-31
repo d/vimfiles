27f560ec970aa5f5627d6cd53e229e74d77955c6
Jesse Zhang <sbjesse@gmail.com>
2015-01-30 16:52:18 -0800

    Builds Command-T

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,6 +3,11 @@
 set -u
 set -e
 
+pushd bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
+popd
+
 git submodule update --init --recursive
 bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
 find bundle/YouCompleteMe -name 'sh.pyc' -delete
