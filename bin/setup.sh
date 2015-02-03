3f6b6e95314ed71562c388a5e3a2642f9bfa82f9
Jesse Zhang <sbjesse@gmail.com>
2015-02-03 12:06:28 -0800

    Supports running setup from outside of ~/.vim

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,11 +3,20 @@
 set -u
 set -e
 
-pushd bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
-popd
+main() {
+	local whereami=$(dirname "${BASH_SOURCE[0]}")
+	local vimdir=$(dirname "${whereami}")
 
-git submodule update --init --recursive
-bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
-find bundle/YouCompleteMe -name 'sh.pyc' -delete
+	pushd "${vimdir}/bundle/command-t/ruby/command-t"
+	ruby extconf.rb
+	make
+	popd
+
+	pushd ${vimdir}
+	git submodule update --init --recursive
+	bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
+	find bundle/YouCompleteMe -name 'sh.pyc' -delete
+	popd ${vimdir}
+}
+
+main
